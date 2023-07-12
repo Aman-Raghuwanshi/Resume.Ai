18b8423fe153e872b2e6cd62ec4783d7f355556e
Aman-Raghuwanshi <137314371+Aman-Raghuwanshi@users.noreply.github.com>
2023-07-12 23:26:50 +0530

    Update DataToJson.js

--- ResumeMakerServer/DataToJson.js
+++ ResumeMakerServer/DataToJson.js
@@ -1,8 +1,11 @@
 function DataToJson(formData) {
+  // Extract and process skills array
   const skillsArray = formData.skills.map(skill => skill.skills.trim()).filter(skill => skill !== '');
 
+  // Initialize education array
   const education = [];
 
+  // Process education data
   const educationData = formData['group-c'];
   for (let i = 0; i < educationData.length; i++) {
     const entry = educationData[i];
@@ -16,17 +19,20 @@ function DataToJson(formData) {
     }
   }
 
+  // Process experience data
   const experience = formData.Experience.map(entry => ({
     CompanyName: entry.companyname.trim(),
     Year: entry.exp_year.trim(),
     Description: entry.exp_description.trim()
   })).filter(entry => entry.CompanyName !== '' && entry.Year !== '' && entry.Description !== '');
 
+  // Process achievements data
   const achievements = formData.Achievements.map(entry => ({
     Type: entry.achievmenttype.trim(),
     Description: entry.achievmentdescription.trim()
   })).filter(entry => entry.Type !== '' && entry.Description !== '');
 
+  // Convert to JSON string
   const jsonData = JSON.stringify({
     Name: formData.firstName || "",
     LastName: formData.lastName || "",
@@ -41,9 +47,8 @@ function DataToJson(formData) {
     Achievements: achievements.length > 0 ? achievements : []
   });
 
-  console.log(jsonData);
-
   return jsonData;
 }
 
+// Export the function
 module.exports = { DataToJson };
